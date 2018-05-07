657a6dab8c78a47fd65b3b92628dd13cc94b46d4
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-07 19:40:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
       FROM students 
       WHERE grade < 12
     SQL
-    DB[:conn].execute(sql)map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
