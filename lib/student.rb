5e7e67c36b82b2b05b690cc5ca0db2674b6f9248
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-05-07 19:46:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
       SELECT *
       FROM students 
       WHERE grade = 10
-      LIMIT ?
+      LIMIT #{number_or_students}
     SQL
      DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
